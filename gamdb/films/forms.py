cd6b387e787fa952a199329a23fc035f8ee4ec91
jakub.sveda <jakub.sveda@icloud.com>
2023-05-09 09:31:55 +0200

    update

--- gamdb/films/forms.py
+++ gamdb/films/forms.py
@@ -2,6 +2,15 @@ from django import forms
 
 
 class CommentForm(forms.Form):
-    author = forms.CharField(required=False)
-    text = forms.CharField(required=False, widget=forms.Textarea(attrs={"rows": 5}))
-    rating = forms.IntegerField(required=False)
+    author = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Pepa z Depa"})
+    )
+    text = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={"rows": 5, "class": "form-control"})
+    )
+    rating = forms.IntegerField(
+        required=False,
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
